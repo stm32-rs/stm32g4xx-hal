99536cc6e7258d91a0ba6955bfe15dc046ea79c7
Vitaly Domnikov <dotcypress@gmail.com>
2019-11-21 15:02:35 -0800

    cleanup

--- .gdbinit
+++ .gdbinit
@@ -1,7 +1,7 @@
 target remote :3333
 
-# monitor itm port 0 on
 # monitor tpiu config internal /tmp/itm.fifo uart off 2000000
+# monitor itm port 0 on
 
 set print asm-demangle on
 monitor arm semihosting enable
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 #![no_std]
 #![allow(non_camel_case_types)]
 
-// #[cfg(not(any(
-//   feature = "stm32g431",
-//   feature = "stm32g441",
-//   feature = "stm32g491",
-//   feature = "stm32g473",
-//   feature = "stm32g483",
-//   feature = "stm32g474",
-//   feature = "stm32g484",
-// )))]
-// compile_error!("This crate requires one of the following features enabled: stm32g431, stm32g441, stm32g491, stm32g473, stm32g483, stm32g474 or stm32g484");
+#[cfg(not(any(
+  feature = "stm32g431",
+  feature = "stm32g441",
+  feature = "stm32g491",
+  feature = "stm32g473",
+  feature = "stm32g483",
+  feature = "stm32g474",
+  feature = "stm32g484",
+)))]
+compile_error!("This crate requires one of the following features enabled: stm32g431, stm32g441, stm32g491, stm32g473, stm32g483, stm32g474 or stm32g484");
 
 extern crate bare_metal;
 extern crate void;
@@ -18,15 +18,34 @@ extern crate void;
 pub extern crate cortex_m;
 pub extern crate embedded_hal as hal;
 pub extern crate nb;
-// pub extern crate stm32g4;
+pub extern crate stm32g4;
 
 pub use nb::block;
 
-// #[cfg(feature = "stm32g..")]
-// pub use stm32g4::stm32g4x1 as stm32;
+#[cfg(feature = "stm32g431")]
+pub use stm32g4::stm32g431 as stm32;
 
-// #[cfg(feature = "rt")]
-// pub use crate::stm32::interrupt;
+#[cfg(feature = "stm32g441")]
+pub use stm32g4::stm32g441 as stm32;
+
+#[cfg(feature = "stm32g491")]
+pub use stm32g4::stm32g491 as stm32;
+
+#[cfg(feature = "stm32g473")]
+pub use stm32g4::stm32g473 as stm32;
+
+#[cfg(feature = "stm32g483")]
+pub use stm32g4::stm32g483 as stm32;
+
+#[cfg(feature = "stm32g474")]
+pub use stm32g4::stm32g474 as stm32;
+
+#[cfg(feature = "stm32g484")]
+pub use stm32g4::stm32g484 as stm32;
+
+
+#[cfg(feature = "rt")]
+pub use crate::stm32::interrupt;
 
 // pub mod adc;
 // pub mod crc;
