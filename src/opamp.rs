83026c03b2d6d85b84af66f6dfd104b2a90a8113
Geens <niels.geens@gmail.com>
2021-02-07 17:49:59 +0100

    Documentation + fixes

--- src/opamp.rs
+++ src/opamp.rs
@@ -1,3 +1,50 @@
+#![deny(missing_docs)]
+
+//! Integrated opamps.
+//! ```
+//! #![no_std]
+//! #![no_main]
+//!
+//! extern crate panic_halt;
+//!
+//! use stm32g4xx_hal::prelude::*;
+//! use stm32g4xx_hal::gpio::gpioa::*;
+//! use stm32g4xx_hal::gpio::gpiob::*;
+//! use stm32g4xx_hal::gpio::Analog;
+//!
+//! #[cortex_m_rt::entry]
+//! fn main() -> ! {
+//!     // take peripherals
+//!     let dp = stm32g4xx_hal::stm32::Peripherals::take().unwrap();
+//!
+//!     // setup clock
+//!     let config = stm32g4xx_hal::rcc::Config::hsi();
+//!     let mut rcc = dp.RCC.freeze(config);
+//!
+//!     // split gpio
+//!     let gpioa = dp.GPIOA.split(&mut rcc);
+//!     let gpiob = dp.GPIOB.split(&mut rcc);
+//!
+//!     // setup opamps
+//!     let (opamp1, opamp2, opamp3, opamp4, _opamp5, _opamp6) = dp.OPAMP.split(&mut rcc);
+//!
+//!     let opamp1 = opamp1.follower(gpioa.pa1, Some(gpioa.pa2));
+//!     let opamp2 = opamp2.follower(gpioa.pa7, Option::<PA6<Analog>>::None);
+//!
+//!     let opamp3 = opamp3.open_loop(gpiob.pb0, gpiob.pb2, Some(gpiob.pb1));
+//!     let opamp4 = opamp4.open_loop(gpiob.pb11, gpiob.pb10, Option::<PB12<Analog>>::None);
+//!
+//!     // disable opamps
+//!     let (_opamp1, _pa1, _some_pa2) = opamp1.disable();
+//!     let (_opamp2, _pa7, _none) = opamp2.disable();
+//!
+//!     let (_opamp3, _pb0, _pb2, _some_pb1) = opamp3.disable();
+//!     let (_opamp4, _pb11, _pb10, _none) = opamp4.disable();
+//!
+//!     loop {}
+//! }
+//! ```
+
 macro_rules! opamps {
     {
         $(
@@ -35,81 +82,104 @@ macro_rules! opamps {
         $(,)?
     } => {
         paste::paste!{
+            /// Re-exports usefull traits.
             pub mod prelude {
                 $(pub use super::$opamp::IntoFollower as _;)*
+                $(pub use super::$opamp::IntoOpenLoop as _;)*
             }
 
             $(
+                /// States for opampX. 
                 pub mod $opamp {
+
+                    /// State type for disabled opamp.
                     pub struct Disabled;
 
+                    /// State type for opamp running in voltage follower mode.
                     pub struct Follower<Input> {
                         input: Input,
                         output: Option<$output>,
                     }
 
+                    /// State type for opamp running in open-loop mode.
                     pub struct OpenLoop<NonInverting, Inverting> {
                         non_inverting: NonInverting,
                         inverting: Inverting,
                         output: Option<$output>,
                     }
 
+                    /// Trait for opamps that can be run in follower mode.
                     pub trait IntoFollower <IntoInput, IntoOutput, Input>
                         where
                             IntoOutput: Into<$output>,
                     {
+                        /// Coonfigures the opamp as voltage follower.
                         fn follower(self, input: IntoInput, output: Option<IntoOutput>) -> Follower<Input>;
                     }
 
+                    /// Trait for opamps that can be run in open-loop mode.
                     pub trait IntoOpenLoop <IntoNonInverting, IntoInverting, IntoOutput, NonInverting, Inverting>
                         where
                             IntoOutput: Into<$output>,
                     {
+                        /// Configures the opamp for open-loop operation.
                         fn open_loop(self, non_inverting: IntoNonInverting, inverting: IntoInverting, output: Option<IntoOutput>)
                             -> OpenLoop<NonInverting, Inverting>;
                     }
 
                     impl<Input> Follower<Input> {
+
+                        /// Disables the opamp and returns the resources it held.
                         pub fn disable(self) -> (Disabled, Input, Option<$output>) {
                             unsafe { (*crate::stm32::OPAMP::ptr()).[<$opamp _csr>].reset() }
                             (Disabled, self.input, self.output)
                         }
 
+                        /// Enables the external output pin.
                         pub fn enable_output(&mut self, output:$output) {
                             self.output = Some(output);
                             unsafe {
                                 (*crate::stm32::OPAMP::ptr()).[<$opamp _csr>].write(|w|
-                                    w.opaintoen().bit(true));
+                                    w.opaintoen().bit(false));
                             }
                         }
 
+                        /// Disables the external output.
+                        /// This will connect the opamp output to the internal ADC.
+                        /// If the output was enabled, the output pin is returned.
                         pub fn disable_output(&mut self) -> Option<$output> {
                             unsafe {
                                 (*crate::stm32::OPAMP::ptr()).[<$opamp _csr>].write(|w|
-                                    w.opaintoen().bit(false));
+                                    w.opaintoen().bit(true));
                             }
                             self.output.take()
                         }
                     }
 
                     impl<NonInverting, Inverting> OpenLoop<NonInverting, Inverting> {
-                        pub fn disable(self) -> (Disabled, NonInverting, Inverting) {
+
+                        /// Disables the opamp and returns the resources it held.
+                        pub fn disable(self) -> (Disabled, NonInverting, Inverting, Option<$output>) {
                             unsafe { (*crate::stm32::OPAMP::ptr()).[<$opamp _csr>].reset() }
-                            (Disabled, self.non_inverting, self.inverting)
+                            (Disabled, self.non_inverting, self.inverting, self.output)
                         }
 
+                        /// Enables the external output pin.
                         pub fn enable_output(&mut self, output:$output) {
                             self.output = Some(output);
                             unsafe {
                                 (*crate::stm32::OPAMP::ptr()).[<$opamp _csr>].write(|w|
-                                    w.opaintoen().bit(true));
+                                    w.opaintoen().bit(false));
                             }
                         }
 
+                        /// Disables the external output.
+                        /// This will connect the opamp output to the internal ADC.
+                        /// If the output was enabled, the output pin is returned.
                         pub fn disable_output(&mut self) -> Option<$output> {
                             unsafe {
                                 (*crate::stm32::OPAMP::ptr()).[<$opamp _csr>].write(|w|
-                                    w.opaintoen().bit(false));
+                                    w.opaintoen().bit(true));
                             }
                             self.output.take()
                         }
@@ -121,7 +191,9 @@ macro_rules! opamps {
                 }
             )*
 
+            /// Extension trait for the OPAMP block.
             pub trait OpampEx {
+                /// Splits the OPAMP block into independant access objects.
                 fn split(
                     self,
                     rcc: &mut crate::rcc::Rcc,
@@ -184,15 +256,13 @@ macro_rules! opamps {
                                     .bits($input_mask)
                                     .vm_sel()
                                     .bits(0b11)
+                                    .opaintoen()
+                                    .bit(output.is_none())
                                     .opaen()
                                     .bit(true)
                             );
                     }
-                    let mut res = Follower {input, output: None};
-                    if let Some(output) = output {
-                        res.enable_output(output);
-                    }
-                    res
+                    Follower {input, output}
                 }
             })*
         }
@@ -253,15 +323,13 @@ macro_rules! opamps {
                                     .bits($non_inverting_mask)
                                     .vm_sel()
                                     .bits($inverting_mask)
+                                    .opaintoen()
+                                    .bit(output.is_none())
                                     .opaen()
                                     .bit(true)
                             );
                     }
-                    let mut res = OpenLoop {non_inverting, inverting, output: None};
-                    if let Some(output) = output {
-                        res.enable_output(output);
-                    }
-                    res
+                    OpenLoop {non_inverting, inverting, output}
                 }
             })*
         }
