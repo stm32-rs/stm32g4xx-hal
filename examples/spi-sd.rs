3051c2bcf17a1bd2e31d1369c309bb93b6cffb3a
Albin Hedman <albin9604@gmail.com>
2023-06-05 21:46:52 +0200

    fmt

--- examples/spi-sd.rs
+++ examples/spi-sd.rs
@@ -17,12 +17,9 @@ use hal::spi;
 use hal::stm32::Peripherals;
 use stm32g4xx_hal as hal;
 
-use embedded_sdmmc::{
-    TimeSource, Timestamp,
-};
+use embedded_sdmmc::{TimeSource, Timestamp};
 
 use cortex_m_rt::entry;
-
 
 #[macro_use]
 mod utils;
