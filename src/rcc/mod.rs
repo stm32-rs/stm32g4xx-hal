f483a211ef751d17b111b841bdcc766a1dad39d1
Albin Hedman <albin.hedman@dectron.se>
2023-09-14 10:08:34 +0200

    Start work on voltage scaling

--- src/rcc/mod.rs
+++ src/rcc/mod.rs
@@ -1,4 +1,4 @@
-use crate::pwr::{PowerConfiguration, self};
+use crate::pwr::{self, PowerConfiguration};
 use crate::stm32::{rcc, FLASH, PWR, RCC};
 use crate::time::{Hertz, RateExtU32};
 
@@ -75,7 +75,7 @@ pub struct Rcc {
 
 impl Rcc {
     /// Apply clock configuration
-    pub fn freeze(self, rcc_cfg: Config, pwr_cfg: PowerConfiguration) -> Self {
+    pub fn freeze(mut self, rcc_cfg: Config, pwr_cfg: PowerConfiguration) -> Self {
         let pll_clk = self.config_pll(rcc_cfg.pll_cfg);
 
         let (sys_clk, sw_bits) = match rcc_cfg.sys_mux {
@@ -95,8 +95,6 @@ impl Rcc {
                 (pll_clk.r.unwrap(), 0b11)
             }
         };
-
-        debug_assert!(sys_clk < Hertz::MHz(150) || (sys_clk < Hertz::MHz(170) && rcc_cfg.enable_boost));
 
         let sys_freq = sys_clk.raw();
         let (ahb_freq, ahb_psc_bits) = match rcc_cfg.ahb_psc {
@@ -125,47 +123,70 @@ impl Rcc {
             _ => (sys_freq, 0b000),
         };
 
-        // Calculate wait states depending on voltage scale and sys_freq
-        //
-        // See 'Number of wait states according to CPU clock (HCLK) frequency' in RM0440
-        let latency = match pwr_cfg.vos() {
-            pwr::VoltageScale::Range1 { enable_boost: true } => {
-                match sys_freq {
-                    0..=34_000_000 => 0b0000,
-                    34_000_001..=68_000_000 => 0b0001,
-                    68_000_001..=102_000_000 => 0b0010,
-                    102_000_001..=136_000_000 => 0b0011,
-                    136_000_001..=170_000_000 => 0b0100,
-                    170_000_001.. => panic!("Too high f_sys: {}, max with voltage scale in 'range1 boost mode' is: 170MHz", sys_freq)
-                }
-            },
-            pwr::VoltageScale::Range1 { enable_boost: false } => {
-                match sys_freq {
-                    0..=30_000_000 => 0b0000,
-                    30_000_001..=60_000_000 => 0b0001,
-                    60_000_001..=90_000_000 => 0b0010,
-                    90_000_001..=120_000_000 => 0b0011,
-                    120_000_001..=150_000_000 => 0b0100,
-                    150_000_001.. => panic!("Too high f_sys: {}, max with voltage scale in 'range1 normal mode' is: 150MHz", sys_freq)
-                }
-            },
-            pwr::VoltageScale::Range2 => {
-                match sys_freq {
-                    0..=12_000_000 => 0b0000,
-                    12_000_001..=24_000_000 => 0b0001,
-                    24_000_001..=26_000_000 => 0b0010,
-                    26_000_001.. => panic!("Too high f_sys: {}, max with voltage scale in 'range2' is: 26MHz", sys_freq)
-                }
-            },
-        };
+        let present_vos_mode = pwr::current_vos();
+        let target_vos_mode = pwr_cfg.vos();
 
-        unsafe {
-            // Adjust flash wait states
-            let flash = &(*FLASH::ptr());
-            flash.acr.modify(|_, w| {
-                w.latency().bits(latency)
-            })
+        match (present_vos_mode, target_vos_mode) {
+            // From VoltageScale::Range1 boost
+            (
+                pwr::VoltageScale::Range1 { enable_boost: true },
+                pwr::VoltageScale::Range1 { enable_boost: true },
+            ) => (), // No change
+            (
+                pwr::VoltageScale::Range1 { enable_boost: true },
+                pwr::VoltageScale::Range1 {
+                    enable_boost: false,
+                },
+            ) => todo!(),
+            (pwr::VoltageScale::Range1 { enable_boost: true }, pwr::VoltageScale::Range2) => {
+                todo!()
+            }
+
+            // From VoltageScale::Range1 normal
+            (
+                pwr::VoltageScale::Range1 {
+                    enable_boost: false,
+                },
+                pwr::VoltageScale::Range1 { enable_boost: true },
+            ) => {
+                self.range1_normal_to_boost(
+                    &pwr_cfg,
+                    sys_freq,
+                    apb1_psc_bits,
+                    apb2_psc_bits,
+                    sw_bits,
+                    ahb_psc_bits,
+                );
+            }
+            (
+                pwr::VoltageScale::Range1 {
+                    enable_boost: false,
+                },
+                pwr::VoltageScale::Range1 {
+                    enable_boost: false,
+                },
+            ) => (), // No change
+            (
+                pwr::VoltageScale::Range1 {
+                    enable_boost: false,
+                },
+                pwr::VoltageScale::Range2,
+            ) => todo!(),
+
+            // From VoltageScale::Range2
+            (pwr::VoltageScale::Range2, pwr::VoltageScale::Range1 { enable_boost: true }) => {
+                todo!()
+            }
+            (
+                pwr::VoltageScale::Range2,
+                pwr::VoltageScale::Range1 {
+                    enable_boost: false,
+                },
+            ) => todo!(),
+            (pwr::VoltageScale::Range2, pwr::VoltageScale::Range2) => (), // No change
         }
+
+        Self::configure_wait_states(&pwr_cfg, sys_freq);
 
         self.rb.cfgr.modify(|_, w| unsafe {
             w.hpre()
@@ -296,6 +317,96 @@ impl Rcc {
             q: q.map(|q| q.0),
             p: p.map(|p| p.0),
         }
+    }
+
+    fn configure_wait_states(pwr_cfg: &PowerConfiguration, sys_freq: u32) {
+        // Calculate wait states depending on voltage scale and sys_freq
+        //
+        // See 'Number of wait states according to CPU clock (HCLK) frequency' in RM0440
+        let latency = match pwr_cfg.vos() {
+            pwr::VoltageScale::Range1 { enable_boost: true } => match sys_freq {
+                0..=34_000_000 => 0b0000,
+                34_000_001..=68_000_000 => 0b0001,
+                68_000_001..=102_000_000 => 0b0010,
+                102_000_001..=136_000_000 => 0b0011,
+                136_000_001..=170_000_000 => 0b0100,
+                170_000_001.. => panic!(
+                    "Too high f_sys: {}, max with voltage scale in 'range1 boost mode' is: 170MHz",
+                    sys_freq
+                ),
+            },
+            pwr::VoltageScale::Range1 {
+                enable_boost: false,
+            } => match sys_freq {
+                0..=30_000_000 => 0b0000,
+                30_000_001..=60_000_000 => 0b0001,
+                60_000_001..=90_000_000 => 0b0010,
+                90_000_001..=120_000_000 => 0b0011,
+                120_000_001..=150_000_000 => 0b0100,
+                150_000_001.. => panic!(
+                    "Too high f_sys: {}, max with voltage scale in 'range1 normal mode' is: 150MHz",
+                    sys_freq
+                ),
+            },
+            pwr::VoltageScale::Range2 => match sys_freq {
+                0..=12_000_000 => 0b0000,
+                12_000_001..=24_000_000 => 0b0001,
+                24_000_001..=26_000_000 => 0b0010,
+                26_000_001.. => panic!(
+                    "Too high f_sys: {}, max with voltage scale in 'range2' is: 26MHz",
+                    sys_freq
+                ),
+            },
+        };
+
+        unsafe {
+            // Adjust flash wait states
+            let flash = &(*FLASH::ptr());
+            flash.acr.modify(|_, w| w.latency().bits(latency))
+        }
+    }
+
+    fn range1_normal_to_boost(
+        &mut self,
+        pwr_cfg: &PowerConfiguration,
+        sys_freq: u32,
+        apb1_psc_bits: u8,
+        apb2_psc_bits: u8,
+        sw_bits: u8,
+        ahb_psc_bits: u8,
+    ) {
+        // (From RM0440 chapter "Power control (PWR)")
+        // The sequence to switch from Range11 normal mode to Range1 boost mode is:
+        // 1. The system clock must be divided by 2 using the AHB prescaler before switching to a
+        // higher system frequency.
+        self.rb.cfgr.modify(|_, w| unsafe { w.hpre().bits(0b1000) });
+        while self.rb.cfgr.read().hpre().bits() != 0b1000 {}
+
+        // 2. Clear the R1MODE bit is in the PWR_CR5 register.
+        unsafe { pwr::set_boost(true) };
+
+        // 3. Adjust the number of wait states according to the new frequency target in range1 boost mode
+        Self::configure_wait_states(pwr_cfg, sys_freq);
+
+        // 4. Configure and switch to new system frequency.
+        self.rb.cfgr.modify(|_, w| unsafe {
+            w.ppre1()
+                .bits(apb1_psc_bits)
+                .ppre2()
+                .bits(apb2_psc_bits)
+                .sw()
+                .bits(sw_bits)
+        });
+
+        while self.rb.cfgr.read().sws().bits() != sw_bits {}
+
+        // 5. Wait for at least 1us and then reconfigure the AHB prescaler to get the needed HCLK
+        // clock frequency.
+
+        // TODO: Do we really need to wait another 1us or is that included in the wait loop in step 1?
+        self.rb
+            .cfgr
+            .modify(|_, w| unsafe { w.hpre().bits(ahb_psc_bits) });
     }
 
     pub(crate) fn enable_hsi(&self) {
