76b212557a89610357ebf401baa3bdf66011e362
Albin Hedman <albin9604@gmail.com>
2023-11-05 21:35:48 +0100

    EEV

---
+++ examples/hrtim_eev.rs
@@ -0,0 +1,111 @@
+//This example puts the timer in PWM mode using the specified pin with a frequency of 100Hz and a duty cycle of 50%.
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use fugit::ExtU32;
+use hal::gpio::gpioa::PA8;
+use hal::gpio::Alternate;
+use hal::gpio::AF13;
+use hal::prelude::*;
+use hal::pwm::hrtim::EventSource;
+use hal::pwm::hrtim::FaultAction;
+use hal::pwm::hrtim::HrCompareRegister;
+use hal::pwm::hrtim::HrPwmAdvExt;
+use hal::pwm::hrtim::HrTimer;
+use hal::pwm::hrtim::Pscl4;
+use hal::pwm::hrtim::{HrControltExt, HrOutput};
+use hal::pwm::FaultMonitor;
+use hal::rcc;
+use hal::stm32;
+use stm32g4xx_hal as hal;
+//mod utils;
+
+use defmt_rtt as _; // global logger
+use panic_probe as _;
+
+#[entry]
+fn main() -> ! {
+    let dp = stm32::Peripherals::take().expect("cannot take peripherals");
+    let cp = stm32::CorePeripherals::take().expect("cannot take core");
+    // Set system frequency to 16MHz * 75/4/2 = 150MHz
+    // This would lead to HrTim running at 150MHz * 32 = 4.8GHz...
+    let mut rcc = dp.RCC.freeze(rcc::Config::pll().pll_cfg(rcc::PllConfig {
+        mux: rcc::PLLSrc::HSI,
+        n: rcc::PllNMul::MUL_75,
+        m: rcc::PllMDiv::DIV_4,
+        r: Some(rcc::PllRDiv::DIV_2),
+        ..Default::default()
+    }));
+
+    let mut delay = cp.SYST.delay(&rcc.clocks);
+
+    let gpioa = dp.GPIOA.split(&mut rcc);
+    let gpiob = dp.GPIOB.split(&mut rcc);
+    let (mut fault_control, flt_inputs, eev_inputs) =
+        dp.HRTIM_COMMON.hr_control(&mut rcc).wait_for_calibration();
+
+    let eev_input3 = eev_inputs
+        .eev_input3
+        .bind_pin(gpiob.pb7.into_pull_down_input())
+        .polarity(hal::pwm::Polarity::ActiveHigh)
+        .finalize(&mut fault_control);
+
+    // ...with a prescaler of 4 this gives us a HrTimer with a tick rate of 1.2GHz
+    // With max the max period set, this would be 1.2GHz/2^16 ~= 18kHz...
+    let prescaler = Pscl4;
+
+    let pin_a: PA8<Alternate<AF13>> = gpioa.pa8.into_alternate();
+
+    //        .               .               .  *
+    //        .  33%          .               .  *            .               .
+    //        .-----.         .-----.         .--.            .               .
+    //out1    |     |         |     |         |  |            .               .
+    //        |     |         |     |         |  |            .               .
+    //   ------     -----------     -----------  -----------------------------------
+    //        .               .               .  *            .               .
+    //        .               .               .  *            .               .
+    //        .               .               .  *--------    .               .
+    //fault   .               .               .  |        |   .               .
+    //        .               .               .  |        |   .               .
+    //   -----------------------------------------        --------------------------
+    //        .               .               .  *            .               .
+    //        .               .               .  *            .               .
+    let (timer, (mut cr1, _cr2, _cr3, _cr4), mut out1) = dp
+        .HRTIM_TIMA
+        .pwm_advanced(pin_a, &mut rcc)
+        .prescaler(prescaler)
+        .period(0xFFFF)
+        //.with_fault_source(fault_source1)
+        //.with_fault_source(fault_source2)
+        .with_fault_source(fault_source3) // Set fault source
+        //.with_fault_source(fault_source4)
+        //.with_fault_source(fault_source5)
+        //.with_fault_source(fault_source6)
+        .fault_action1(FaultAction::ForceInactive)
+        .fault_action2(FaultAction::ForceInactive)
+        // alternated every period with one being
+        // inactive and the other getting to output its wave form
+        // as normal
+        .finalize(&mut fault_control);
+
+    out1.enable_rst_event(EventSource::Cr1); // Set low on compare match with cr1
+    out1.enable_set_event(EventSource::Period); // Set high at new period
+    cr1.set_duty(timer.get_period() / 3);
+    //unsafe {((HRTIM_COMMON::ptr() as *mut u8).offset(0x14) as *mut u32).write_volatile(1); }
+    out1.enable();
+
+    defmt::info!("Started");
+
+    loop {
+        for _ in 0..5 {
+            delay.delay(500_u32.millis());
+            defmt::info!("State: {}", out1.get_state());
+        }
+        if fault_control.fault_3.is_fault_active() {
+            fault_control.fault_3.clear_fault(); // Clear fault every 5s
+            out1.enable();
+            defmt::info!("failt cleared, and output reenabled");
+        }
+    }
+}
--- src/hrtim/external_event.rs
+++ src/hrtim/external_event.rs
@@ -1,76 +1,31 @@
 use core::marker::PhantomData;
 
-use stm32g4::stm32g474::HRTIM_COMMON;
+use crate::comparator::{COMP1, COMP2, COMP3, COMP4, COMP5, COMP6};
+use crate::gpio::gpioa::{PA12, PA15};
+use crate::gpio::gpiob::{PB0, PB10, PB11};
+use crate::gpio::gpioc::{PC10, PC7};
+use crate::gpio::{self, AF13, AF3};
+use crate::hrtim::control::HrPwmControl;
+use crate::pwm::FaultMonitor;
+use crate::stm32::HRTIM_COMMON;
 
-use crate::comparator::{self, Comparator, COMP1, COMP2, COMP4, COMP6, COMP3, COMP5, COMP7};
 
-use super::{control::HrTimCalibrated, event::EventSource};
-
-#[derive(Copy, Clone, PartialEq)]
-pub enum ExternalEventSource {
-    Eevnt1 { _x: PhantomData<()> },
-    Eevnt2 { _x: PhantomData<()> },
-    Eevnt3 { _x: PhantomData<()> },
-    Eevnt4 { _x: PhantomData<()> },
-    Eevnt5 { _x: PhantomData<()> },
-    Eevnt6 { _x: PhantomData<()> },
-    Eevnt7 { _x: PhantomData<()> },
-    Eevnt8 { _x: PhantomData<()> },
-    Eevnt9 { _x: PhantomData<()> },
-    Eevnt10 { _x: PhantomData<()> },
+pub unsafe trait FaultSource: Copy {
+    const ENABLE_BITS: u8;
 }
 
-impl<PSCL, DST> From<ExternalEventSource> for EventSource<PSCL, DST> {
-    fn from(val: ExternalEventSource) -> Self {
-        EventSource::ExternalEvent(val)
-    }
+pub struct SourceBuilder<I> {
+    _input: I,
+    src_bits: u8,
+
+    /// FLTxP
+    is_active_high: bool,
+
+    /// FLTxF[3:0]
+    filter_bits: u8,
 }
 
-pub trait ExternalEventExt<const EEV_N: u8, const SRC: u8> {
-    fn external_event(&self) -> ExternalEventBuilder<EEV_N, SRC>;
-}
-
-macro_rules! impl_eev_ext_comp {
-    ($N:literal: $comp:ident) => {
-        impl<E: comparator::EnabledState> ExternalEventExt<$N, 0b10> for Comparator<$comp, E> {
-            fn external_event(&self) -> ExternalEventBuilder<$N, 0b10> {
-                ExternalEventBuilder {
-                    edge_or_polarity_bits: 0, // Polarity sensitive
-                    polarity_bit: false,      // Active high
-                    filter_bits: 0,           // No filter
-                    fast_bit: false,          // Not fast
-                }
-            }
-        }
-    };
-}
-
-impl_eev_ext_comp!(1: COMP2);
-impl_eev_ext_comp!(2: COMP4);
-impl_eev_ext_comp!(3: COMP6);
-impl_eev_ext_comp!(4: COMP1);
-impl_eev_ext_comp!(5: COMP3);
-impl_eev_ext_comp!(6: COMP2);
-impl_eev_ext_comp!(7: COMP4);
-impl_eev_ext_comp!(8: COMP6);
-impl_eev_ext_comp!(9: COMP5);
-impl_eev_ext_comp!(10: COMP7);
-
-impl_eev_ext_pin!(1: PC12);
-impl_eev_ext_pin!(2: PC11);
-impl_eev_ext_pin!(3: PB7);
-impl_eev_ext_pin!(4: PB6);
-impl_eev_ext_pin!(5: PB9);
-impl_eev_ext_pin!(6: PB5);
-impl_eev_ext_pin!(7: PB4);
-impl_eev_ext_pin!(8: PB8);
-impl_eev_ext_pin!(9: PB3);
-impl_eev_ext_pin!(10: PC5);
-impl_eev_ext_pin!(10: PC6);
-
-
-
-pub struct ExternalEventBuilder<const N: u8, const SRC: u8> {
+pub struct SourceBuilder<const N: u8, const SRC: u8> {
     /// EExSNS
     edge_or_polarity_bits: u8,
 
@@ -84,200 +39,211 @@ pub struct ExternalEventBuilder<const N: u8, const SRC: u8> {
     fast_bit: bool,
 }
 
-pub enum EdgeOrPolarity {
-    Edge(Edge),
-    Polarity(Polarity),
+impl<I> SourceBuilder<I> {
+    unsafe fn new(input: I, src_bits: u8) -> Self {
+        SourceBuilder {
+            _input: input,
+            src_bits,
+            is_active_high: false,
+            filter_bits: 0b0000,
+        }
+    }
 }
 
-pub enum Edge {
-    Rising = 0b01,
-    Falling = 0b10,
-    Both = 0b11,
+macro_rules! impl_faults {
+    ($(
+        $input:ident => $source:ident:
+            PINS=[($pin:ident, $af:ident), $(($pin_b:ident, $af_b:ident),)*],
+            COMP=$compX:ident, $enable_bits:literal,
+            $fltinrZ:ident, $fltWsrc_0:ident, $fltWsrc_1:ident, $fltWp:ident, $fltWf:ident, $fltWe:ident, $fltWlck:ident,
+    )+) => {$(
+
+        // This should NOT be Copy/Clone
+        pub struct $input {
+            pub(crate) _x: PhantomData<()>
+        }
+
+        #[derive(Copy, Clone)]
+        pub struct $source {
+            _x: PhantomData<()>
+        }
+
+        impl $input {
+            pub fn bind_pin<IM>(self, pin: $pin<gpio::Input<IM>>) -> SourceBuilder<$input> {
+                pin.into_alternate::<$af>();
+                unsafe { SourceBuilder::new(self, 0b00) }
+            }
+
+            $(
+                pub fn bind_pin_b<IM>(self, pin: $pin_b<gpio::Input<IM>>) -> SourceBuilder<$input> {
+                    pin.into_alternate::<$af_b>();
+                    unsafe { SourceBuilder::new(self, 0b00) }
+                }
+            )*
+
+            pub fn bind_comp(self, _comp: &crate::comparator::Comparator<$compX, crate::comparator::Enabled>) -> SourceBuilder<$input> {
+                unsafe { SourceBuilder::new(self, 0b01) }
+            }
+
+            /*pub fn bind_external(?) {
+                SourceBuilder::new(self, 0b10);
+            }*/
+        }
+
+        impl SourceBuilder<$input> {
+            pub fn finalize(self, _control: &mut HrPwmControl) -> $source {
+                let SourceBuilder{ _input, src_bits, is_active_high, filter_bits } = self;
+
+                // Setup fault source
+                unsafe {
+                    let common = &*HRTIM_COMMON::ptr();
+
+                    common.fltinr2.modify(|_r, w| w.$fltWsrc_1().bit(src_bits & 0b10 != 0));
+                    common.$fltinrZ.modify(|_r, w| w
+                        .$fltWsrc_0().bit(src_bits & 0b01 != 0)
+                        .$fltWp().bit(is_active_high)
+                        .$fltWf().bits(filter_bits)
+                        .$fltWe().set_bit() // Enable
+                    );
+
+                    // ... and lock configuration
+                    common.$fltinrZ.modify(|_r, w| w.$fltWlck().set_bit());
+                }
+
+                $source {
+                    _x: PhantomData
+                }
+            }
+
+            pub fn polarity(mut self, polarity: super::Polarity) -> Self {
+                self.is_active_high = polarity == super::Polarity::ActiveHigh;
+                self
+            }
+
+            // TODO: add more settings
+            /* pub fn blanking(?) -> Self */
+
+            pub fn filter(mut self, filter: FaultSamplingFilter) -> Self {
+                self.filter_bits = filter as u8;
+                self
+            }
+        }
+
+        unsafe impl FaultSource for $source {
+            const ENABLE_BITS: u8 = $enable_bits;
+        }
+    )+}
 }
 
-pub enum Polarity {
-    ActiveHigh,
-    ActiveLow,
+// TODO: Lookup to ensure the alternate function are the same for other devices than stm32g474
+#[cfg(feature = "stm32g474")]
+impl_faults!(
+    FaultInput1 => FaultSource1: PINS=[(PA12, AF13),], COMP=COMP2, 0b000001, fltinr1, flt1src, flt1src_1, flt1p, flt1f, flt1e, flt1lck,
+    FaultInput2 => FaultSource2: PINS=[(PA15, AF13),], COMP=COMP4, 0b000010, fltinr1, flt2src, flt2src_1, flt2p, flt2f, flt2e, flt2lck,
+    FaultInput3 => FaultSource3: PINS=[(PB10, AF13),], COMP=COMP6, 0b000100, fltinr1, flt3src, flt3src_1, flt3p, flt3f, flt3e, flt3lck,
+    FaultInput4 => FaultSource4: PINS=[(PB11, AF13),], COMP=COMP1, 0b001000, fltinr1, flt4src, flt4src_1, flt4p, flt4f, flt4e, flt4lck,
+    FaultInput5 => FaultSource5: PINS=[(PB0, AF13), (PC7, AF3),], COMP=COMP3, 0b010000, fltinr2, flt5src, flt5src_1, flt5p, flt5f, flt5e, flt5lck,
+    FaultInput6 => FaultSource6: PINS=[(PC10, AF13),], COMP=COMP5, 0b100000, fltinr2, flt6src_0, flt6src_1, flt6p, flt6f, flt6e, flt6lck,
+);
+
+pub struct FaultInputs {
+    pub fault_input1: FaultInput1,
+    pub fault_input2: FaultInput2,
+    pub fault_input3: FaultInput3,
+    pub fault_input4: FaultInput4,
+    pub fault_input5: FaultInput5,
+    pub fault_input6: FaultInput6,
 }
 
-pub enum EevSamplingFilter {
+pub enum FaultSamplingFilter {
     /// No filtering, fault acts asynchronously
     ///
-    /// Note that this bypasses any f_eevs (FaultSamplingClkDiv)
+    /// Note that this bypasses any f_flts (SamplingClkDiv)
     None = 0b0000,
 
     /// Sample directly at rate f_hrtim, with a count of 2
     ///
-    /// Note that this bypasses: any f_eevs (FaultSamplingClkDiv)
+    /// Note that this bypasses: any f_flts (SamplingClkDiv)
     HrtimN2 = 0b0001,
 
     /// Sample directly at rate f_hrtim, with a count of 4
     ///
-    /// Note that this bypasses any f_eevs (FaultSamplingClkDiv)
+    /// Note that this bypasses any f_flts (SamplingClkDiv)
     HrtimN4 = 0b0010,
 
     /// Sample directly at rate f_hrtim, with a count of 8
     ///
-    /// Note that this bypasses any f_eevs (FaultSamplingClkDiv)
+    /// Note that this bypasses any f_flts (SamplingClkDiv)
     HrtimN8 = 0b0011,
 
-    /// Sample at rate f_eevs / 2, with a count of 6
-    EevsDiv2N6 = 0b0100,
+    /// Sample at rate f_flts / 2, with a count of 6
+    FltsDiv2N6 = 0b0100,
 
-    /// Sample at rate f_eevs / 2, with a count of 8
-    EevsDiv2N8 = 0b0101,
+    /// Sample at rate f_flts / 2, with a count of 8
+    FltsDiv2N8 = 0b0101,
 
-    /// Sample at rate f_eevs / 4, with a count of 6
-    EevsDiv4N6 = 0b0110,
+    /// Sample at rate f_flts / 4, with a count of 6
+    FltsDiv4N6 = 0b0110,
 
-    /// Sample at rate f_eevs / 4, with a count of 8
-    EevsDiv4N8 = 0b0111,
+    /// Sample at rate f_flts / 4, with a count of 8
+    FltsDiv4N8 = 0b0111,
 
-    /// Sample at rate f_eevs / 8, with a count of 6
-    EevsDiv8N6 = 0b1000,
+    /// Sample at rate f_flts / 8, with a count of 6
+    FltsDiv8N6 = 0b1000,
 
-    /// Sample at rate f_eevs / 8, with a count of 8
-    EevsDiv8N8 = 0b1001,
+    /// Sample at rate f_flts / 8, with a count of 8
+    FltsDiv8N8 = 0b1001,
 
-    /// Sample at rate f_eevs / 16, with a count of 5
-    EevsDiv16N5 = 0b1010,
+    /// Sample at rate f_flts / 16, with a count of 5
+    FltsDiv16N5 = 0b1010,
 
-    /// Sample at rate f_eevs / 16, with a count of 6
-    EevsDiv16N6 = 0b1011,
+    /// Sample at rate f_flts / 16, with a count of 6
+    FltsDiv16N6 = 0b1011,
 
-    /// Sample at rate f_eevs / 16, with a count of 8
-    EevsDiv16N8 = 0b1100,
+    /// Sample at rate f_flts / 16, with a count of 8
+    FltsDiv16N8 = 0b1100,
 
-    /// Sample at rate f_eevs / 32, with a count of 5
-    EevsDiv32N5 = 0b1101,
+    /// Sample at rate f_flts / 32, with a count of 5
+    FltsDiv32N5 = 0b1101,
 
-    /// Sample at rate f_eevs / 32, with a count of 6
-    EevsDiv32N6 = 0b1110,
+    /// Sample at rate f_flts / 32, with a count of 6
+    FltsDiv32N6 = 0b1110,
 
-    /// Sample at rate f_eevs / 32, with a count of 8
-    EevsDiv32N8 = 0b1111,
+    /// Sample at rate f_flts / 32, with a count of 8
+    FltsDiv32N8 = 0b1111,
 }
 
-pub trait ExternalEventBuilder1To5 {}
-pub trait ExternalEventBuilder6To10 {}
+macro_rules! impl_flt_monitor {
+    ($($t:ident: ($fltx:ident, $fltxc:ident),)+) => {$(
+        pub struct $t {
+            pub(crate) _x: PhantomData<()>
+        }
 
-impl<const N: u8, const SRC: u8> ExternalEventBuilder<N, SRC> {
-    pub fn edge_or_polarity(mut self, edge_or_polarity: EdgeOrPolarity) -> Self {
-        (self.edge_or_polarity_bits, self.polarity_bit) = match edge_or_polarity {
-            EdgeOrPolarity::Polarity(Polarity::ActiveHigh) => (0b00, false),
-            EdgeOrPolarity::Polarity(Polarity::ActiveLow) => (0b00, true),
-            EdgeOrPolarity::Edge(Edge::Rising) => (0b01, false),
-            EdgeOrPolarity::Edge(Edge::Falling) => (0b10, false),
-            EdgeOrPolarity::Edge(Edge::Both) => (0b11, false),
-        };
-
-        self
-    }
-}
-
-impl<const N: u8, const SRC: u8> ExternalEventBuilder<N, SRC>
-where
-    ExternalEventBuilder<N, SRC>: ExternalEventBuilder1To5,
-{
-    pub fn fast(mut self) -> Self {
-        self.fast_bit = true;
-        self
-    }
-}
-
-impl<const N: u8, const SRC: u8> ExternalEventBuilder<N, SRC>
-where
-    ExternalEventBuilder<N, SRC>: ExternalEventBuilder6To10,
-{
-    pub fn filter(mut self, filter: EevSamplingFilter) -> Self {
-        self.filter_bits = filter as _;
-        self
-    }
-}
-
-pub trait ExternalEventToEventSource {
-    fn finalize<PSCL, TIM>(self, _calibrated: &mut HrTimCalibrated) -> ExternalEventSource;
-}
-
-macro_rules! impl_eev1_5_to_es {
-    ($eev:ident, $N:literal, $eeXsrc:ident, $eeXpol:ident, $eeXsns:ident, $eeXfast:ident) => {
-        impl<const SRC: u8> ExternalEventBuilder1To5 for ExternalEventBuilder<$N, SRC> {}
-
-        impl<const SRC: u8> ExternalEventToEventSource for ExternalEventBuilder<$N, SRC> {
-            fn finalize<PSCL, TIM>(self, _calibrated: &mut HrTimCalibrated) -> ExternalEventSource {
-                let ExternalEventBuilder {
-                    edge_or_polarity_bits,
-                    polarity_bit,
-                    filter_bits: _,
-                    fast_bit,
-                } = self;
-
+        impl FaultMonitor for $t {
+            fn is_fault_active(&self) -> bool {
                 let common = unsafe { &*HRTIM_COMMON::ptr() };
+                common.isr.read().$fltx().bit()
+            }
 
-                // SAFETY: Thanks to, `HrTimCalibrated`, we know we have exclusive access to the register,
-                //         we also know no timers are started.
-                unsafe {
-                    common.eecr1.modify(|_r, w| {
-                        w.$eeXsrc()
-                            .bits(SRC)
-                            .$eeXpol()
-                            .bit(polarity_bit)
-                            .$eeXsns()
-                            .bits(edge_or_polarity_bits)
-                            .$eeXfast()
-                            .bit(fast_bit)
-                    });
-                }
+            fn clear_fault(&mut self) {
+                let common = unsafe { &*HRTIM_COMMON::ptr() };
+                common.icr.write(|w| w.$fltxc().set_bit());
+            }
 
-                ExternalEventSource::$eev { _x: PhantomData }
+            // TODO: Should we have our own trait since it does not seem possible to implement this
+            fn set_fault(&mut self) {
+                todo!()
             }
         }
-    };
+    )+};
 }
 
-macro_rules! impl_eev6_10_to_es {
-    ($eev:ident, $N:literal, $eeXsrc:ident, $eeXpol:ident, $eeXsns:ident, $eeXf:ident) => {
-        impl<const SRC: u8> ExternalEventBuilder6To10 for ExternalEventBuilder<$N, SRC> {}
-
-        impl<const SRC: u8> ExternalEventToEventSource for ExternalEventBuilder<$N, SRC> {
-            fn finalize<PSCL, TIM>(self, _calibrated: &mut HrTimCalibrated) -> ExternalEventSource {
-                let ExternalEventBuilder {
-                    edge_or_polarity_bits,
-                    polarity_bit,
-                    filter_bits,
-                    fast_bit: _,
-                } = self;
-
-                let common = unsafe { &*HRTIM_COMMON::ptr() };
-
-                // SAFETY: Thanks to, `HrTimCalibrated`, we know we have exclusive access to the register,
-                //         we also know no timers are started.
-                unsafe {
-                    common.eecr2.modify(|_r, w| {
-                        w.$eeXsrc()
-                            .bits(SRC)
-                            .$eeXpol()
-                            .bit(polarity_bit)
-                            .$eeXsns()
-                            .bits(edge_or_polarity_bits)
-                    });
-                    common.eecr3.modify(|_r, w| w.$eeXf().bits(filter_bits));
-                }
-
-                ExternalEventSource::$eev { _x: PhantomData }
-            }
-        }
-    };
-}
-
-impl_eev1_5_to_es!(Eevnt1, 1, ee1src, ee1pol, ee1sns, ee1fast);
-impl_eev1_5_to_es!(Eevnt2, 2, ee2src, ee2pol, ee2sns, ee2fast);
-impl_eev1_5_to_es!(Eevnt3, 3, ee3src, ee3pol, ee3sns, ee3fast);
-impl_eev1_5_to_es!(Eevnt4, 4, ee4src, ee4pol, ee4sns, ee4fast);
-impl_eev1_5_to_es!(Eevnt5, 5, ee5src, ee5pol, ee5sns, ee5fast);
-
-impl_eev6_10_to_es!(Eevnt6, 6, ee6src, ee6pol, ee6sns, ee6f);
-impl_eev6_10_to_es!(Eevnt7, 7, ee7src, ee7pol, ee7sns, ee7f);
-impl_eev6_10_to_es!(Eevnt8, 8, ee8src, ee8pol, ee8sns, ee8f);
-impl_eev6_10_to_es!(Eevnt9, 9, ee9src, ee9pol, ee9sns, ee9f);
-impl_eev6_10_to_es!(Eevnt10, 10, ee10src, ee10pol, ee10sns, ee10f);
+impl_flt_monitor!(
+    FltMonitorSys: (sysflt, sysfltc),
+    FltMonitor1: (flt1, flt1c),
+    FltMonitor2: (flt2, flt2c),
+    FltMonitor3: (flt3, flt3c),
+    FltMonitor4: (flt4, flt4c),
+    FltMonitor5: (flt5, flt5c),
+    FltMonitor6: (flt6, flt6c),
+);
