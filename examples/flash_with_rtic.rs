stm32-rs/stm32g4xx-hal

commit e8e9c2d311b5b0fe1d648c0fe4b402251a636ba8

Pwr (#95)
* NOT DONE add pwr config

* Start work on voltage scaling

* PWR allow dead code for function that will be used to change voltage range once allowed

* Forgot to add mod pwr import

* Fix examples for the RCC PWR change

* PWR - Boost mode inverted

examples/uart.rs
@@ -6,12 +6,13 @@
 use core::fmt::Write;
 
 use hal::prelude::*;
+use hal::pwr::PwrExt;
 use hal::serial::FullConfig;
 use hal::{rcc, stm32};
 use stm32g4xx_hal as hal;
 
 use cortex_m_rt::entry;
-use log::info;
+use defmt::info;
 use nb::block;
 
 #[macro_use]
@@ -23,18 +24,19 @@ fn main() -> ! {
 
     info!("start");
     let dp = stm32::Peripherals::take().expect("cannot take peripherals");
-    let mut rcc = dp.RCC.freeze(rcc::Config::hsi());
+    let pwr = dp.PWR.constrain().freeze();
+    let mut rcc = dp.RCC.freeze(rcc::Config::hsi(), pwr);
 
     info!("Init UART");
     // on Nucleo-G474, the pins marked TX/RX are connected to USART1 by default, whereas USART2 is
     // connected to the ST-Link's UART.
-    let gpioa = dp.GPIOA.split(&mut rcc);
+    /*let gpioa = dp.GPIOA.split(&mut rcc);
     let tx = gpioa.pa2.into_alternate();
     let rx = gpioa.pa3.into_alternate();
     let mut usart = dp
         .USART2
         .usart(tx, rx, FullConfig::default(), &mut rcc)
-        .unwrap();
+        .unwrap();*/
     /*let gpioc = dp.GPIOC.split(&mut rcc);
     let tx = gpioc.pc4.into_alternate();
     let rx = gpioc.pc5.into_alternate();
@@ -43,7 +45,15 @@ fn main() -> ! {
         .usart(tx, rx, FullConfig::default(), &mut rcc)
         .unwrap();*/
 
-    writeln!(usart, "Hello USART1\r\n").unwrap();
+    let gpioc = dp.GPIOC.split(&mut rcc);
+    let tx = gpioc.pc10.into_alternate();
+    let rx = gpioc.pc11.into_alternate();
+    let mut usart = dp
+        .USART3
+        .usart(tx, rx, FullConfig::default(), &mut rcc)
+        .unwrap();
+
+    writeln!(usart, "Hello USART3, yay!!\r\n").unwrap();
 
     let mut cnt = 0;
     loop {

examples/adc-continious-dma.rs
@@ -11,6 +11,7 @@ use crate::hal::{
     delay::SYSTDelayExt,
     dma::{config::DmaConfig, stream::DMAExt, TransferExt},
     gpio::GpioExt,
+    pwr::PwrExt,
     rcc::{Config, RccExt},
     stm32::Peripherals,
 };
@@ -33,7 +34,8 @@ fn main() -> ! {
 
     info!("rcc");
     let rcc = dp.RCC.constrain();
-    let mut rcc = rcc.freeze(Config::hsi());
+    let pwr = dp.PWR.constrain().freeze();
+    let mut rcc = rcc.freeze(Config::hsi(), pwr);
 
     let streams = dp.DMA1.split(&rcc);
     let config = DmaConfig::default()

examples/adc-continious.rs
@@ -8,6 +8,7 @@ use crate::hal::{
     },
     delay::SYSTDelayExt,
     gpio::GpioExt,
+    pwr::PwrExt,
     rcc::{Config, RccExt},
     stm32::Peripherals,
 };
@@ -15,7 +16,7 @@ use stm32g4xx_hal as hal;
 
 use cortex_m_rt::entry;
 
-use log::info;
+use utils::logger::info;
 
 #[macro_use]
 mod utils;
@@ -32,7 +33,8 @@ fn main() -> ! {
     info!("rcc");
 
     let rcc = dp.RCC.constrain();
-    let mut rcc = rcc.freeze(Config::hsi());
+    let pwr = dp.PWR.constrain().freeze();
+    let mut rcc = rcc.freeze(Config::hsi(), pwr);
 
     info!("Setup Gpio");
     let gpioa = dp.GPIOA.split(&mut rcc);

examples/adc-one-shot-dma.rs
@@ -11,6 +11,7 @@ use crate::hal::{
     delay::SYSTDelayExt,
     dma::{config::DmaConfig, stream::DMAExt, TransferExt},
     gpio::GpioExt,
+    pwr::PwrExt,
     rcc::{Config, RccExt},
     stm32::Peripherals,
 };
@@ -33,7 +34,8 @@ fn main() -> ! {
     info!("rcc");
 
     let rcc = dp.RCC.constrain();
-    let mut rcc = rcc.freeze(Config::hsi());
+    let pwr = dp.PWR.constrain().freeze();
+    let mut rcc = rcc.freeze(Config::hsi(), pwr);
 
     let mut streams = dp.DMA1.split(&rcc);
     let config = DmaConfig::default()

examples/adc-one-shot.rs
@@ -2,8 +2,9 @@
 #![no_std]
 
 use crate::hal::{
-    adc::{config::SampleTime, AdcClaim, ClockSource},
+    adc::{config::SampleTime, AdcClaim},
     delay::SYSTDelayExt,
+    pwr::PwrExt,
     rcc::Config,
     stm32::Peripherals,
 };
@@ -29,26 +30,31 @@ fn main() -> ! {
     info!("rcc");
 
     let rcc = dp.RCC.constrain();
-    let mut rcc = rcc.freeze(Config::hsi());
+    let pwr = dp.PWR.constrain().freeze();
+    let mut rcc = rcc.freeze(Config::hsi(), pwr);
 
     info!("Setup Adc1");
     let mut delay = cp.SYST.delay(&rcc.clocks);
-    let mut adc = dp
-        .ADC1
-        .claim(ClockSource::SystemClock, &rcc, &mut delay, true);
+    let mut adc = dp.ADC2.claim_and_configure(
+        stm32g4xx_hal::adc::ClockSource::SystemClock,
+        &rcc,
+        stm32g4xx_hal::adc::config::AdcConfig::default(),
+        &mut delay,
+        false,
+    );
 
     info!("Setup Gpio");
 
     let gpioa = dp.GPIOA.split(&mut rcc);
-    let pa0 = gpioa.pa0.into_analog();
+    let pa7 = gpioa.pa7.into_analog();
 
     info!("Enter Loop");
 
     loop {
         info!("Convert");
-        let sample = adc.convert(&pa0, SampleTime::Cycles_640_5);
+        let sample = adc.convert(&pa7, SampleTime::Cycles_640_5);
         info!("sample to mv");
         let millivolts = adc.sample_to_millivolts(sample);
-        info!("pa3: {}mV", millivolts);
+        info!("pa7: {}mV", millivolts);
     }
 }

examples/blinky_delay.rs
@@ -5,6 +5,7 @@
 
 use hal::delay::DelayFromCountDownTimer;
 use hal::prelude::*;
+use hal::pwr::PwrExt;
 use hal::rcc::Config;
 use hal::stm32;
 use hal::time::ExtU32;
@@ -24,7 +25,8 @@ fn main() -> ! {
     info!("start");
     let dp = stm32::Peripherals::take().expect("cannot take peripherals");
     let cp = cortex_m::Peripherals::take().expect("cannot take core peripherals");
-    let mut rcc = dp.RCC.freeze(Config::hsi());
+    let pwr = dp.PWR.constrain().freeze();
+    let mut rcc = dp.RCC.freeze(Config::hsi(), pwr);
 
     info!("Init Led");
     let gpioa = dp.GPIOA.split(&mut rcc);

examples/can-echo.rs
@@ -5,6 +5,7 @@ use crate::hal::{
     can::CanExt,
     gpio::{GpioExt as _, Speed},
     nb::block,
+    pwr::PwrExt,
     rcc::{Config, RccExt, SysClockSrc},
     stm32::Peripherals,
     time::RateExtU32,
@@ -46,8 +47,10 @@ fn main() -> ! {
 
     let dp = Peripherals::take().unwrap();
     let _cp = cortex_m::Peripherals::take().expect("cannot take core peripherals");
+
+    let pwr = dp.PWR.constrain().freeze();
     let rcc = dp.RCC.constrain();
-    let mut rcc = rcc.freeze(Config::new(SysClockSrc::HSE(24.MHz())));
+    let mut rcc = rcc.freeze(Config::new(SysClockSrc::HSE(24.MHz())), pwr);
 
     info!("Split GPIO");
 

examples/clockout.rs
@@ -9,6 +9,7 @@ extern crate panic_halt;
 extern crate stm32g4xx_hal as hal;
 
 use hal::prelude::*;
+use hal::pwr::PwrExt;
 use hal::rcc::{Config, LSCOSrc, MCOSrc, Prescaler};
 use hal::stm32;
 use rt::entry;
@@ -16,7 +17,8 @@ use rt::entry;
 #[entry]
 fn main() -> ! {
     let dp = stm32::Peripherals::take().expect("cannot take peripherals");
-    let mut rcc = dp.RCC.freeze(Config::hsi());
+    let pwr = dp.PWR.constrain().freeze();
+    let mut rcc = dp.RCC.freeze(Config::hsi(), pwr);
     let gpioa = dp.GPIOA.split(&mut rcc);
 
     let lsco = gpioa.pa2.lsco(LSCOSrc::LSI, &mut rcc);

commit 75289a50ca9d70be4ea9cc442bf52f19d485c3a9

Fix examples broken by #95

examples/flash_with_rtic.rs
@@ -13,6 +13,7 @@ mod app {
     use stm32g4xx_hal::flash::{FlashExt, FlashSize, FlashWriter, Parts};
     use stm32g4xx_hal::prelude::*;
     use stm32g4xx_hal::rcc::{PllConfig, RccExt};
+    use stm32g4xx_hal::pwr::PwrExt;
 
     const LOG_LEVEL: log::LevelFilter = log::LevelFilter::Info;
 
@@ -67,7 +68,8 @@ mod app {
         // The ADC will ultimately be put into synchronous mode and will derive
         // its clock from the AHB bus clock, with a prescalar of 2 or 4.
 
-        let mut rcc = rcc.freeze(clock_config);
+        let pwr = dp.PWR.constrain().freeze();
+        let mut rcc = rcc.freeze(clock_config, pwr);
 
         unsafe {
             let mut flash = &(*stm32g4xx_hal::stm32::FLASH::ptr());

examples/uart.rs
@@ -12,7 +12,7 @@ use hal::{rcc, stm32};
 use stm32g4xx_hal as hal;
 
 use cortex_m_rt::entry;
-use defmt::info;
+use utils::logger::info;
 use nb::block;
 
 #[macro_use]