81760bd8389fc5055776c09421fef07f04b9372d
Albin Hedman <albin.hedman@dectron.se>
2024-04-19 10:10:10 +0200

    HRTIM - Allow using capture module with DMA

--- examples/hrtim/capture-dma.rs
+++ examples/hrtim/capture-dma.rs
@@ -18,11 +18,12 @@ fn main() -> ! {
     use stm32g4xx_hal as hal;
 
     use hal::{
+        dma::{config::DmaConfig, stream::DMAExt, TransferExt},
         gpio::{gpioa::PA8, Alternate, GpioExt, AF13},
         hrtim::{
-            capture::HrCapture, compare_register::HrCompareRegister, control::HrControltExt,
-            external_event, external_event::ToExternalEventSource, output::HrOutput,
-            timer::HrSlaveTimerCpt, timer::HrTimer, HrPwmAdvExt, Pscl128,
+            capture, compare_register::HrCompareRegister, control::HrControltExt, external_event,
+            external_event::ToExternalEventSource, output::HrOutput, timer::HrSlaveTimerCpt,
+            timer::HrTimer, HrPwmAdvExt, Pscl128,
         },
         pwr::PwrExt,
         rcc::{self, RccExt},
@@ -64,6 +65,9 @@ fn main() -> ! {
     // With max the max period set, this would be 30MHz/2^16 ~= 458Hz...
     let prescaler = Pscl128;
 
+    //        t1     t2       .
+    //        |      |        .
+    //        v      v        .
     //        .               .
     //        .  50%          .
     //         ------          ------
@@ -78,44 +82,52 @@ fn main() -> ! {
         .eev_input6
         .bind(input)
         .edge_or_polarity(external_event::EdgeOrPolarity::Edge(
-            external_event::Edge::Falling,
+            external_event::Edge::Both,
         ))
         .finalize(&mut hr_control);
 
     let mut hr_control = hr_control.constrain();
-    let (mut timer, (mut cr1, _cr2, _cr3, _cr4), mut out1) = dp
+    let (timer, (mut cr1, _cr2, _cr3, _cr4), mut out1) = dp
         .HRTIM_TIMA
         .pwm_advanced(pin_a, &mut rcc)
         .prescaler(prescaler)
         .period(period)
         .finalize(&mut hr_control);
-
     out1.enable_rst_event(&cr1); // Set low on compare match with cr1
     out1.enable_set_event(&timer); // Set high at new period
-
     cr1.set_duty(period / 2);
+
+    let (mut timer, mut capture, _capture_ch2) = timer.split_capture();
     timer.start(&mut hr_control);
     out1.enable();
 
-    let capture = timer.capture_ch1();
     capture.enable_interrupt(true, &mut hr_control);
     capture.add_event(&eev_input6);
+
+    info!("Setup DMA");
+    let streams = dp.DMA1.split(&rcc);
+    let config = DmaConfig::default()
+        .transfer_complete_interrupt(false)
+        .circular_buffer(true)
+        .memory_increment(true);
+
+    let first_buffer = cortex_m::singleton!(: [u32; 16] = [0; 16]).unwrap();
+    let mut transfer = streams.0.into_circ_peripheral_to_memory_transfer(
+        capture.enable_dma(),
+        &mut first_buffer[..],
+        config,
+    );
+
+    transfer.start(|_| ());
 
     let mut old_duty = 0;
     loop {
         for duty in (u32::from(period) / 10)..(9 * u32::from(period) / 10) {
-            if !capture.is_pending() {
-                continue;
-            }
-            let value = capture.get_signed();
+            let mut data = [0; 2];
+            transfer.read_exact(&mut data);
+            let [t1, t2] = data.map(capture::dma_value_to_signed);
             cr1.set_duty(duty as u16);
-            capture.clear_interrupt();
-            info!(
-                "Capture: {:?}, duty: {}, diff: {}",
-                value,
-                old_duty,
-                value - old_duty as i32
-            );
+            info!("Capture: t1: {}, t2: {}, duty: {}, ", t1, t2, old_duty);
             old_duty = duty;
         }
     }
