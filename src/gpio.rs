47af3ae24c6ed45e383ce478ec49ea206daa9b6c
Adin Ackerman <adinackerman@gmail.com>
2024-09-17 10:18:56 -0700

    ...

--- src/gpio.rs
+++ src/gpio.rs
@@ -475,7 +475,7 @@ macro_rules! gpio {
                     }
 
                     /// Configures the pin as external trigger
-                    pub fn listen(self, edge: SignalEdge, exti: &mut EXTI) -> $PXi<Input<MODE>> {
+                    pub fn listen(self, edge: SignalEdge, exti: &mut EXTI) -> $PXi<MODE> {
                         exti.listen(Event::from_code($i), edge);
                         $PXi { _mode: PhantomData }
                     }
