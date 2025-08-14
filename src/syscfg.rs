eb3aef7144cbe89d1e9b8509a2b7202b7ed6f2ef
boondockenergy <matt@boondocker.io>
2025-08-13 17:36:56 -0700

    Add I2C fast mode plus enable function to SysCfg. Added I2C fast mode plus example using an AS5600 magnetic angle sensor

--- src/syscfg.rs
+++ src/syscfg.rs
@@ -27,3 +27,22 @@ impl Deref for SysCfg {
         &self.0
     }
 }
+
+impl SysCfg {
+    /// Enable/disable I2C fast mode plus on the I2C bus index provided as a const generic parameter.
+    ///
+    /// Pins that are configured as I2C alternate functions will be configured as fast mode plus.
+    /// The alternate function mode of the pin must be set before FMP is enabled in SysCfg.
+    ///
+    /// When FM+ mode is activated on a pin, the GPIO speed configuration (OSPEEDR) is is ignored and overridden.
+    ///
+    pub fn i2c_fmp_enable<const BUS: u8>(&mut self, en: bool) {
+        match BUS {
+            1 => (*self).cfgr1().modify(|_, w| w.i2c1_fmp().bit(en)),
+            2 => (*self).cfgr1().modify(|_, w| w.i2c2_fmp().bit(en)),
+            3 => (*self).cfgr1().modify(|_, w| w.i2c3_fmp().bit(en)),
+            4 => (*self).cfgr1().modify(|_, w| w.i2c4_fmp().bit(en)),
+            _ => panic!("Invalid I2C bus"),
+        };
+    }
+}
